b4f403f8a81e85cf78c18a58b074d362b48eba5e
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-06-05 01:09:53 +0800

    Add gameboard methods: playerMove and reset

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,7 +6,18 @@ function createPlayer(name, symbol){
 const gameBoard = (function createBoard(){
     const board = new Array(9).fill(undefined)
 
+    function playerMove(code, playerSymbol){
+        if (code >= 0 || code < 9){
+            board[code] = playerSymbol
+        }
+        else{
+            throw Error("invalid playerMove code")
+        }
+    }
 
+    function reset(){
+        board.fill(undefined)
+    }
 })();
 
 const Game = (function createGameboard(){
