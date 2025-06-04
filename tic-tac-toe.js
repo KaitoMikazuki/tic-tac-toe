bf0798e852bbed6d9210473ec7c1c9fc91086b77
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-06-05 01:09:11 +0800

    Moved gameBoard out of game module

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,25 @@
-const Game = (function createGameboard(){
-    const board = new Array(9).fill(undefined);
+// Create player
+function createPlayer(name, symbol){
+    return {name, symbol}
+}
 
+const gameBoard = (function createBoard(){
+    const board = new Array(9).fill(undefined)
+
+
+})();
+
+const Game = (function createGameboard(){
+    const state = {
+        gameBoard: gameBoard,
+        player1: null,
+        player2: null,
+    };
+    
+    function startGame(){
+        state.player1 = createPlayer("UserInput1", "X");
+        state.player2 = createPlayer("UserInput1", "O");
+    }
     return {}
 })();
+
